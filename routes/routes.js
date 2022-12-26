c7b30778ecaafa15544ebcc3492f5e29079b82da
Davg1700 <davg1700@gmail.com>
2022-12-26 10:40:19 -0700

    added read me

--- routes/routes.js
+++ routes/routes.js
@@ -1,62 +1,44 @@
-const fs = require('fs');
-const uuid = require('../helpers/uuid')
-const router = require('express').Router();
+const fs = require("fs");
+const uuid = require("../helpers/uuid");
+const router = require("express").Router();
 
 //gets notes from get req
-router.get('/notes', (req,res) => {
-    console.log("Fetching notes.");
-    fs.readFile('./db/db.json', 'utf8', (err,data) => {
-        if (err) {
-            console.error(err)
-        } else {
-            return res.json(JSON.parse(data));
-        }
-    })
+router.get("/notes", (req, res) => {
+  console.log("Fetching notes.");
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      return res.json(JSON.parse(data));
+    }
+  });
 });
 
 // adds notes to post req.
-router.post('/notes', (req,res) => {
-    console.log("Updating Kremlin.");
-    fs.readFile('./db/db.json', 'utf8', (err,data) => {
-        let notes = JSON.parse(data);
+router.post("/notes", (req, res) => {
+  console.log("Updating Kremlin.");
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    let notes = JSON.parse(data);
 
-        const {title, text} = req.body;
+    const { title, text } = req.body;
 
-        if(req.body) {
-            const newNote = {
-                title,
-                text,
-                id: uuid(),
-            };
-            notes.push(newNote);
+    if (req.body) {
+      const newNote = {
+        title,
+        text,
+        id: uuid(),
+      };
+      notes.push(newNote);
 
-            fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
-                if (err) {
-                    console.error(err);
-                } else {
-                    res.json(notes);
-                }
-            });
+      fs.writeFile("./db/db.json", JSON.stringify(notes), (err) => {
+        if (err) {
+          console.error(err);
+        } else {
+          res.json(notes);
         }
-    });
+      });
+    }
+  });
 });
 
-//delets notes
-router.delete('/notes/:id', (req, res) => {
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-        let notes = JSON.parse(data);
-        
-        notes = notes.filter(note => (note.id !== req.params.id));
-
-        fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log('Note deleted!');
-                res.json(notes);
-            }
-        });
-
-    });
-})
 module.exports = router;
